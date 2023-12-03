feb49e8f0c4d70636d6af1dcc8fa651852407599
yasmine-sassi <147749339+yasmine-sassi@users.noreply.github.com>
2023-12-03 16:09:50 +0100

    Delete tpjava directory

--- tpjava/Bank/TestBank.java
+++
@@ -1,9 +0,0 @@
-public class TestBank {
-    public static void main(String[] args) {
-        BankAccount b=new BankAccount(123,"yasmine",85213);
-        b.deposit(50); 
-        b.affiche();
-        b.withdraw(1000);
-        System.out.println("remaining: "+b.getBalance());
-    }
-}
