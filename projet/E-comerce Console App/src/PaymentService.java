28df121aeeb3fe239417fb4473a9a77cf7ed2367
yasmine-sassi <147749339+yasmine-sassi@users.noreply.github.com>
2023-12-25 10:16:24 +0100

    Delete projet/E-comerce Console App directory

--- projet/E-comerce Console App/src/PaymentService.java
+++
@@ -1,9 +0,0 @@
-//This class is supposed to check the balance of a bank account
-public class PaymentService {
-    public boolean processPayment(double amount) {
-        System.out.println("Processing payment of " + amount+"DT");
-        boolean paymentSuccess = true; 
-        //paymentSuccess=false if the balance is less than the amount 
-        return paymentSuccess;
-    }
-}
